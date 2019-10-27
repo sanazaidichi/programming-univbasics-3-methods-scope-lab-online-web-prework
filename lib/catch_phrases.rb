deeba20c099c1587349668171a148f68dabcd438
Sana Zaidi <sana.rizvi.5@gmail.com>
2019-10-27 20:54:18 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,9 @@
 def mario 
   phrase = 'It\'s-a me, Mario!'
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  phrase
 end
-mario
+
 
 def toadstool 
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
